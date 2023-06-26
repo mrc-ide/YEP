677794f93205cef0850b50b6458a9eb1a15d4382
KeithJF82 <keith.fraser@imperial.ac.uk>
2023-06-26 19:06:32 +0100

    Added function for creating multiple datasets

--- R/outputs_generate.R
+++ R/outputs_generate.R
@@ -12,7 +12,7 @@
 #'
 #' [TBA - Explanation of breakdown of regions to model and how to set lengths of FOI_values and R0_values]
 #'
-#' @param input_data List of population and vaccination data for multiple regions
+#' @param input_data List of population and vaccination data for multiple regions in standard format [TBA]
 #' @param FOI_values Vector of values of the force of infection due to spillover from sylvatic reservoir
 #' @param R0_values Vector of values of the basic reproduction number for human-human transmission
 #' @param sero_template Seroprevalence data template - data frame with region, year, minimum/maximum age, vc_factor [TBA]
@@ -65,13 +65,24 @@ Generate_Dataset <- function(input_data = list(),FOI_values = c(),R0_values = c(
 
   #Prune input data based on regions
   regions=regions_breakdown(c(sero_template$region,case_template$region))
+  n_regions=length(input_data$region_labels)
   input_data=input_data_truncate(input_data,regions)
 
   #Cross-reference templates with input regions
-  xref_sero=template_region_xref(sero_template,input_data$region_labels)
-  xref_case=template_region_xref(case_template,input_data$region_labels)
-  sero_line_list=xref_sero$line_list
-  case_line_list=xref_case$line_list
+  if(is.null(sero_template)==FALSE){
+    xref_sero=template_region_xref(sero_template,input_data$region_labels)
+    sero_line_list=xref_sero$line_list
+  } else {
+    xref_sero=data.frame(year_data_begin=rep(Inf,n_regions),year_end=rep(-Inf,n_regions))
+    sero_line_list=rep(NA,n_regions)
+  }
+  if(is.null(case_template)==FALSE){
+    xref_case=template_region_xref(case_template,input_data$region_labels)
+    case_line_list=xref_case$line_list
+  } else {
+    xref_case=data.frame(year_data_begin=rep(Inf,n_regions),year_end=rep(-Inf,n_regions))
+    case_line_list=rep(NA,n_regions)
+  }
   year_data_begin=year_end=rep(NA,length(input_data$region_labels))
   for(i in 1:length(year_data_begin)){
     year_data_begin[i]=min(xref_sero$year_data_begin[i],xref_case$year_data_begin[i])
@@ -79,7 +90,6 @@ Generate_Dataset <- function(input_data = list(),FOI_values = c(),R0_values = c(
   }
 
   inv_reps=1/n_reps
-  n_regions=length(input_data$region_labels)
   assert_that(length(FOI_values)==n_regions,msg="Length of FOI_values must match number of regions to be modelled")
   assert_that(length(R0_values)==n_regions,msg="Length of R0_values must match number of regions to be modelled")
   if(mode_start==2){assert_that(length(start_SEIRV)==n_regions,
@@ -218,7 +228,7 @@ Generate_Dataset <- function(input_data = list(),FOI_values = c(),R0_values = c(
 #' @details This function is used to generate serological, data for multiple regions/parameter sets based
 #'   on a template.
 #'
-#' @param input_data List of population and vaccination data for multiple regions
+#' @param input_data List of population and vaccination data for multiple regions in standard format [TBA]
 #' @param FOI_values Values for each region of the force of infection due to spillover from sylvatic reservoir
 #' @param R0_values Values for each region of the basic reproduction number for human-human transmission
 #' @param template Seroprevalence data for comparison, by region, year & age group, in format no. samples/no.
@@ -341,7 +351,7 @@ Generate_Sero_Dataset <- function(input_data = list(),FOI_values = c(),R0_values
 #'
 #' @details This function is used to generate annual case/death data based on a template
 #'
-#' @param input_data List of population and vaccination data for multiple regions
+#' @param input_data List of population and vaccination data for multiple regions in standard format [TBA]
 #' @param FOI_values Values for each region of the force of infection due to spillover from sylvatic reservoir
 #' @param R0_values Values for each region of the basic reproduction number for human-human transmission
 #' @param template Annual reported case/death data for comparison, by region and year, in format no. cases/no.
@@ -634,4 +644,77 @@ Generate_VIMC_Burden_Dataset <- function(input_data = list(), FOI_values = c(), 
 
   return(data.frame(region=template$region,year=template$year,age_min=template$age_min,age_max=template$age_max,
                     cases=model_case_values,dalys=model_dalys_values,deaths=model_death_values,YLL=model_YLL_values))
+}
+#-------------------------------------------------------------------------------
+#' @title Generate_Multiple Datasets
+#'
+#' @description Generate multiple datasets for multiple sets of input parameter values
+#'
+#' @details This function is used to generate annual serological and/or case/death data based on templates for
+#'   multiple sets of parameters (spillover FOI, R0, case/death reporting probabilities, vaccine efficacy)
+#'
+#' @param input_data List of population and vaccination data for multiple regions in standard format [TBA]
+#' @param FOI_values Array of sets of values of the force of infection due to spillover from sylvatic reservoir;
+#'   dimensions [TBA]
+#' @param R0_values Array of values of the basic reproduction number for human-human transmission;
+#'   dimensions [TBA]
+#' @param sero_template Seroprevalence data template - data frame with region, year, minimum/maximum age, vc_factor [TBA]
+#'   and number of samples
+#' @param case_template Annual reported case/death data template - data frame with region and year
+#' @param vaccine_efficacy Vector of values of vaccine efficacy [TBA]
+#' @param p_severe_inf Probability of an infection being severe
+#' @param p_death_severe_inf Probability of a severe infection resulting in death
+#' @param p_rep_severe Vector of values of probability of reporting of a severe but non-fatal infection [TBA]
+#' @param p_rep_death Vetor of value of probability of reporting of a fatal infection [TBA]
+#' @param mode_start Flag indicating how to set initial population immunity level in addition to vaccination
+#'  If mode_start=0, only vaccinated individuals
+#'  If mode_start=1, shift some non-vaccinated individuals into recovered to give herd immunity
+#'  If mode_start=2, use SEIRV input in list from previous run(s)
+#' @param start_SEIRV SEIRV data from end of a previous run to use as input (list of datasets, one per region)
+#' @param dt Time increment in days to use in model (should be either 1.0, 2.5 or 5.0 days)
+#' @param n_reps number of stochastic repetitions
+#' @param deterministic TRUE/FALSE - set model to run in deterministic mode if TRUE
+#' @param mode_parallel Set mode for parallelization, if any:
+#'   If mode_parallel="none", no parallelization
+#'   If mode_parallel="pars_multi", all regions run in parallel for same time period with same output type
+#'   If mode_parallel="clusterMap", all regions run in parallel with different time periods and output types
+#' @param cluster Cluster of threads to use if mode_parallel="clusterMap"
+#' '
+#' @export
+#'
+Generate_Multiple_Datasets <- function(input_data = list(),FOI_values = list(),R0_values = list(),sero_template = NULL,case_template = NULL,
+                                       vaccine_efficacy = c(1.0), p_severe_inf = 0.12, p_death_severe_inf = 0.39, p_rep_severe = c(1.0),
+                                       p_rep_death = c(1.0),mode_start = 1,start_SEIRV = NULL, dt = 1.0,n_reps = 1, deterministic = FALSE,
+                                       mode_parallel = "none",cluster = NULL){
+
+  assert_that(length(dim(FOI_values))==2)
+  assert_that(length(dim(FOI_values))==2)
+  n_param_sets=dim(FOI_values)[2]
+  assert_that(dim(R0_values)[2]==n_param_sets)
+  assert_that(length(vaccine_efficacy)==n_param_sets)
+  assert_that(length(p_rep_severe)==n_param_sets)
+  assert_that(length(p_rep_death)==n_param_sets)
+
+  if(is.null(sero_template)==FALSE){model_sero_values=array(NA,dim=c(nrow(sero_template),n_param_sets))}else(model_sero_values=NULL)
+  if(is.null(case_template)==FALSE){model_case_values=model_death_values=array(NA,dim=c(nrow(case_template),n_param_sets))}else{
+    model_case_values=model_death_values=NULL
+  }
+  cat("\nRunning set:\n")
+  for(set in 1:n_param_sets){
+    cat("\t",set,sep="")
+    dataset_single <- Generate_Dataset(input_data,FOI_values[,set],R0_values[,set],sero_template,case_template,vaccine_efficacy[set],
+                                       p_severe_inf,p_death_severe_inf,p_rep_severe[set],p_rep_death[set],mode_start,start_SEIRV,dt,n_reps,
+                                       deterministic,mode_parallel,cluster,output_frame=FALSE)
+    if(is.null(sero_template)==FALSE){
+      model_sero_values[,set]=dataset_single$model_sero_values
+    }
+    if(is.null(case_template)==FALSE){
+      model_case_values[,set]=dataset_single$model_case_values
+      model_death_values[,set]=dataset_single$model_death_values
+    }
+  }
+  cat("\nDatasets complete.\n")
+
+  return(list(model_sero_values=model_sero_values,model_case_values=model_case_values,
+              model_death_values=model_death_values))
 }
